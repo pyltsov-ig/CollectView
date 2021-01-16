7788ab068fb1e9a1d4759edf6464950f41a36ed4
IGP <pyltsov@gmail.com>
2021-01-16 21:02:59 +0100

    20.11. Немного практики, пункты с 1 по 5

---
+++ CollectView/Cell.swift
@@ -0,0 +1,14 @@
+//
+//  Cell.swift
+//  CollectView
+//
+//  Created by ИГОРЬ on 16/01/2021.
+//
+
+import UIKit
+
+class Cell: UICollectionViewCell {
+    
+    @IBOutlet weak var temperatureImage: UIImageView!
+    
+}
