519b81f35cd1d27b021dc61907c34f7733fd3a1f
IGP <pyltsov@gmail.com>
2021-01-16 22:57:20 +0100

    Пункты с 1 по 4

--- CollectView/Cell.swift
+++ CollectView/Cell.swift
@@ -11,4 +11,8 @@ class Cell: UICollectionViewCell {
     
     @IBOutlet weak var temperatureImage: UIImageView!
     
+    func setTemperatureImage(thermName:String) {
+        temperatureImage! = UIImageView(image: UIImage(named: thermName))
+    }
+    
 }
