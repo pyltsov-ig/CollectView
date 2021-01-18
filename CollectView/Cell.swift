7f7b0633085ad5cadb9f93c467c8078b4b5d9367
IGP <pyltsov@gmail.com>
2021-01-18 12:43:05 +0100

    Проблема с неправильной реакцией на события решена - использовался неверный метод (техническая ошибка)

--- CollectView/Cell.swift
+++ CollectView/Cell.swift
@@ -10,9 +10,13 @@ import UIKit
 class Cell: UICollectionViewCell {
     
     @IBOutlet weak var temperatureImage: UIImageView!
+    @IBOutlet weak var smileImage: UIImageView!
     
     func setTemperatureImage(tempName:String) {
         temperatureImage.image = UIImage(named: tempName)
     }
     
+    func setSmileImage(smileName:String) {
+        smileImage.image = UIImage(named: smileName)
+    }
 }
--- CollectView/ViewController.swift
+++ CollectView/ViewController.swift
@@ -10,7 +10,8 @@ import UIKit
 class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
     
     @IBOutlet weak var collectionViewOne: UICollectionView!
-
+    @IBOutlet weak var collectionViewTwo: UICollectionView!
+    
     
     let arrayTemperature = ["temp.green",
     "temp.blackGreen",
@@ -19,40 +20,75 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     "temp.orange",
     "temp.red"]
     
+    let arraySmiles = ["hate",
+    "bad",
+    "notPleasant",
+    "routine",
+    "nice",
+    "favorite"]
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         collectionViewOne.delegate = self
         collectionViewOne.dataSource = self
-        // Do any additional setup after loading the view.
+        
+        collectionViewTwo.delegate = self
+        collectionViewTwo.dataSource = self
     }
     
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        arrayTemperature.count
+        if collectionView == collectionViewOne {
+            return arrayTemperature.count
+        }
+        if collectionView == collectionViewTwo {
+            return arraySmiles.count
+        }
+        return 0
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CellOne", for: indexPath) as? Cell {
-            let imageName = arrayTemperature[indexPath.row]
-            cell.setTemperatureImage(tempName: imageName)
-            return cell
+
+        if collectionView == collectionViewOne{
+            if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CellOne", for: indexPath) as? Cell {
+                let imageName = arrayTemperature[indexPath.row]
+                cell.setTemperatureImage(tempName: imageName)
+                return cell
+            }
         }
+        
+        if collectionView == collectionViewTwo{
+            if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CellTwo", for: indexPath) as? Cell {
+                let imageName = arraySmiles[indexPath.row]
+                cell.setSmileImage(smileName: imageName)
+                return cell
+            }
+        }
+
+
         return UICollectionViewCell()
     }
     
-    func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
-        print(arrayTemperature[indexPath.row])
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         guard let vc = storyboard?.instantiateViewController(withIdentifier: "ShowImageVC") as?  ShowImageVC else {return}
+        
         var currentSelectedImage:String!
-        currentSelectedImage = arrayTemperature[indexPath.row]
+        
+        if collectionView == collectionViewOne {
+            currentSelectedImage = arrayTemperature[indexPath.row]
+        }
+        if collectionView == collectionViewTwo  {
+            currentSelectedImage = arraySmiles[indexPath.row]
+        }
+        
         vc.setImageName(name: currentSelectedImage)
         present(vc, animated: true, completion: nil)
         
+  
     }
-    
-
 
 
 }
